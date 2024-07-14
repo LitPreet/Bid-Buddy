7f78301c1dde29fcfbc898e1d5d5187912da9b8b
preet006 <preetbhardwaj146@gmail.com>
2024-07-14 21:40:48 +0530

    debugging vercel error

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,28 +5,29 @@ import { HeroHighlight,Highlight } from "@/components/ui/hero-highlight";
 export default  function Home() {
 
   return (
-    <HeroHighlight>
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
-        className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-      >
-        Join the Auction &apos;s Revolution. Unleash Exclusive Deals
-        Post Your Items {" "}
-        <Highlight className="text-black dark:text-white">
-        Let the Bidding Begin!
-        </Highlight>
-      </motion.h1>
-    </HeroHighlight>
+    <h1>hey</h1>
+    // <HeroHighlight>
+    //   <motion.h1
+    //     initial={{
+    //       opacity: 0,
+    //       y: 20,
+    //     }}
+    //     animate={{
+    //       opacity: 1,
+    //       y: [20, -5, 0],
+    //     }}
+    //     transition={{
+    //       duration: 0.5,
+    //       ease: [0.4, 0.0, 0.2, 1],
+    //     }}
+    //     className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+    //   >
+    //     Join the Auction &apos;s Revolution. Unleash Exclusive Deals
+    //     Post Your Items {" "}
+    //     <Highlight className="text-black dark:text-white">
+    //     Let the Bidding Begin!
+    //     </Highlight>
+    //   </motion.h1>
+    // </HeroHighlight>
   );
 }
